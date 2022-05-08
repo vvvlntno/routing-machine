ce0add5cb4696e465f23d6ebf0c40c572e1e6ad7
vvvlntno <vvvlntno@gmail.com>
2022-05-08 12:59:11 +0200

    changed local framework7 path, added wikipediasearch

---
+++ functionality/api-call-wikipedia/wikipediasearch.js
@@ -0,0 +1,49 @@
+async function handleSubmit(event) {
+    event.preventDefault();
+    const inputValue = document.querySelector('.search-input').value;
+    const searchQuery = inputValue.trim();
+  
+    try {
+      const results = await searchWikipedia(searchQuery);
+      displayResults(results);
+    } catch (err) {
+      console.log(err);
+      alert('Failed to search wikipedia');
+    }
+}
+  
+  async function searchWikipedia(searchQuery) {
+    const endpoint = `https://de.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+    return json;
+}
+  
+  function displayResults(results) {
+    const searchResults = document.querySelector('.search-results');
+  
+    results.query.search.forEach(result => {
+      const url = `https://de.wikipedia.org/?curid=${result.pageid}`;
+  
+      searchResults.insertAdjacentHTML(
+        'beforeend',
+        `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+          <span class="result-snippet">${result.snippet}</span><br>
+        </div>`
+      );
+    });
+}
+  
+  
+const form = document.querySelector('.search-wrapper');
+form.addEventListener('submit', handleSubmit);
+  
